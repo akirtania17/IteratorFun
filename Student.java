62a993d6c25257317da77bc152d4c83742677454
akirtania17 <64326700+akirtania17@users.noreply.github.com>
2021-09-15 08:23:43 -0400

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    String name;
+    int id;
+    double gpa;
+    int grade;
+
+    public Student(String n, int id, double gpa, int grade) {
+        name = n;
+        this.id = id;
+        this.gpa = gpa;
+        this.grade = grade;
+    }
+}
